b5612a12cddd0ba577ce5ddcf6bd92203c798dbd
Bayan <baian.009.99@gmail.com>
2024-08-30 19:41:58 +0500

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	// Настройка подключения к базе данных
+	db, err := storage.NewPostgresDB()
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// Инициализация роутера
+	r := chi.NewRouter()
+
+	// Регистрация обработчиков
+	r.Post("/reservations", handler.CreateReservationHandler(db))
+	r.Get("/reservations/{room_id}", handler.GetReservationsHandler(db))
+
+	// Запуск сервера
+	log.Println("Starting server on :8080...")
+	http.ListenAndServe(":8080", r)
+}
